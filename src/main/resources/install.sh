74c93ae231d36cb625422c1713b2cb46f621b2da
mdrudi <massimiliano.drudi@bo.ingv.it>
2014-12-05 17:47:21 +0100

    further update to work on command line

--- src/main/resources/install.sh
+++ src/main/resources/install.sh
@@ -2,9 +2,6 @@ ResourcesDir=`dirname $0`
 
 echo ResourcesDir= $ResourcesDir
 
-#ls $ResourcesDir/../python/sp
-#ls $ResourcesDir/../app-resources/jt_vto
-
 cp -a $ResourcesDir/../app-resources/* .
 mkdir jt_vto
 cp -a $ResourcesDir/../python/sp/* ./jt_vto
@@ -13,3 +10,4 @@ cd jt_vto
 ln -sf sp-ciop.py run.py
 
 echo Remind : before submission to be generated application.xml and inputfiles
+echo NB : to run from command line, change your working dir to jt_vto
